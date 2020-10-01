dddc15d166e3cea0bc6159cee83e270be5942785
Fadhil Ilham <fadhilhmr@gmail.com>
2020-10-01 18:06:29 +0700

    feat: add Model,Controller,service traveloke

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/HotelController.java
@@ -0,0 +1,51 @@
+package apap.tutorial.traveloke.controller;
+
+import apap.tutorial.traveloke.service.HotelService;
+import apap.tutorial.traveloke.model.HotelModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class HotelController {
+    @Autowired
+    private HotelService hotelService;
+
+    @RequestMapping("/Hotel/add")
+    public String addHotel(
+            @RequestParam(value="idHotel", required = true) String idHotel,
+            @RequestParam(value="namaHotel", required = true) String namaHotel,
+            @RequestParam(value="alamat", required = true) String alamat,
+            @RequestParam(value="noTelepon", required = true) String noTelepon,
+            Model model
+    ){
+        HotelModel hotel = new HotelModel(idHotel, namaHotel, alamat, noTelepon);
+
+        hotelService.addHotel(hotel);
+
+        model.addAttribute( "idHotel", idHotel);
+
+        return "add-hotel";
+    }
+    @RequestMapping("/hotel/viewall")
+    public String listHotel(Model model){
+        List<HotelModel> listHotel = hotelService.getHotelList();
+
+        model.addAttribute("listHotel", listHotel);
+        return "viewall-hotel";
+    }
+    @RequestMapping("/hotel/view")
+    public String detailHotel(
+            @RequestParam(value = "idHotel") String idHotel,
+            Model model){
+
+        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
+
+        model.addAttribute("hotel", hotel);
+        return "view-hotel";
+    }
+}
---
+++ traveloke/src/main/java/apap/tutorial/traveloke/service/HotelInMemoryService.java
@@ -0,0 +1,41 @@
+
+package apap.tutorial.traveloke.service;
+
+
+import apap.tutorial.traveloke.model.HotelModel;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HotelInMemoryService implements HotelService {
+
+    private List<HotelModel> listHotel;
+
+    public HotelInMemoryService(){
+        listHotel = new ArrayList<>();
+    }
+
+    @Override
+    public void addHotel(HotelModel hotel){
+        listHotel.add(hotel);
+    }
+
+    @Override
+    public List<HotelModel> getHotelList(){
+        return listHotel;
+    }
+
+    @Override
+    public HotelModel getHotelByIdHotel(String idHotel) {
+        for (int i = 0;i<listHotel.size();i++) {
+            String test = listHotel.get(i).getIdHotel();
+            if (test == idHotel) {
+                return listHotel.get(i);
+            }
+        }
+        return null;
+    }
+
+}
